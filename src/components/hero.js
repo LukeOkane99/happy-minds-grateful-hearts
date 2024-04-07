549b72259cb16549a52375f3fb371ae57cdf234e
LukeOkane99 <theofficiallukeokane@gmail.com>
2024-04-07 16:42:15 +0100

    Adding hero component

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,7 +1,19 @@
 import React from "react";
+import "./hero.css";
+import profile from '../images/profile.jpg'
 
 const Hero = () => {
-  return <div></div>;
+  return (
+    <div className="hero">
+      <img src={profile} alt="profile image"></img>
+      <h1>I'm Amanda, a certified minfulness coach.</h1>
+      <p>Are you ready to get started on your journey?</p>
+      <div className="hero-action">
+        <div className="hero-connect">Connect with me</div>
+        <div className="hero-get-started">Get Started</div>
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,25 +2,27 @@ import React from "react";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import logo from "../images/icon.png";
+import logo from "../images/logo.jpg";
 import "./navbar.css";
 
 const NavigationBar = () => {
   return (
-    <Navbar bg="#30D5C8" fixed="top" data-bs-theme="light" className="navbar">
-      <Container>
-        <div className="logo">
-          <img src={logo} alt="logo"></img>
-        </div>
-        <div className="nav-links">
-          <Nav>
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#about">About</Nav.Link>
-            <Nav.Link href="#classes">Classes</Nav.Link>
-          </Nav>
-        </div>
-      </Container>
-    </Navbar>
+    <div>
+      <Navbar data-bs-theme="light" className="navbar">
+        <Container>
+          <div className="logo">
+            <img src={logo} alt="logo"></img>
+          </div>
+          <div className="nav-links">
+            <Nav>
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#about">About</Nav.Link>
+              <Nav.Link href="#classes">Classes</Nav.Link>
+            </Nav>
+          </div>
+        </Container>
+      </Navbar>
+    </div>
   );
 };
 
