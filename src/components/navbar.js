e39eecc2faa1215862a2a56e6642b1bb98339b50
LukeOkane99 <theofficiallukeokane@gmail.com>
2024-04-20 23:53:34 +0100

    Adding media queries for mobile

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,13 +1,40 @@
-import React from "react";
-import logo from "../images/header-logo.png"
+import React, { useRef } from "react";
+import logo from "../images/header-logo.png";
 import "./navbar.css";
+import menu_toggle from "../images/menu-toggle.svg";
+import menu_close from "../images/menu-close.svg";
+import mob_logo from "../images/mob-logo.png"
 
 const NavigationBar = () => {
+  const menuRef = useRef();
+
+  // When right = 0, menu will be displayed in the small screen.
+  const openMenu = () => {
+    menuRef.current.style.right = "0";
+  };
+
+  const closeMenu = () => {
+    menuRef.current.style.right = "-350px";
+  };
+
   return (
     <div className="navbar">
-      <img src={logo} alt="logo"></img>
-      <ul className="nav-links">
-        <li href="#about">About</li>
+      <img src={logo} alt="logo" className="nav-logo"></img>
+      <img src={mob_logo} alt="" className="nav-mob-logo"></img>
+      <img
+        src={menu_toggle}
+        onClick={openMenu}
+        alt=""
+        className="nav-mob-open"
+      ></img>
+      <ul ref={menuRef} className="nav-links">
+        <img
+          src={menu_close}
+          onClick={closeMenu}
+          alt=""
+          className="nav-mob-close"
+        ></img>
+        <li>About</li>
         <li>Classes</li>
         <li>Contact Me</li>
         <li>Testimonials</li>
